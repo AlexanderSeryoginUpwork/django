13433e48669bd1c376b74f7ced11d73897715b7e
AlexanderSeryoginUpwork <alexander.seryogin.upwork@gmail.com>
2021-01-20 10:53:05 +0200

    part1_7__Модели

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    # black=True т.е. необязательное поле
+    content = models.TextField(blank=True)
+    # auto_now_add только один раз в момент создания
+    created_at = models.DateTimeField(auto_now_add=True)
+    # auto_now обновляет при каждом сохранении
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photos/%y/%m/%d')
+    is_published = models.BooleanField(default=True)
