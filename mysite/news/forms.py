51790e3653ae02775fa0025ea111455fbe77a620
AlexanderSeryoginUpwork <alexander.seryogin.upwork@gmail.com>
2021-01-27 14:35:33 +0200

    part1_26__Кастомные валидаторы

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -2,7 +2,8 @@ from django import forms
 
 from .models import Category
 from .models import News
-
+import re
+from django.core.exceptions import ValidationError
 
 class NewsForm(forms.ModelForm):
     class Meta:
@@ -25,3 +26,15 @@ class NewsForm(forms.ModelForm):
                 }
             ),
         }
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if re.match(r'\d', title):
+            raise ValidationError('Заголовок не должен начинаться с цифры')
+        return title
+
+    def clean_content(self):
+        content = self.cleaned_data['content']
+        if re.match(r'.*123456.*', content):
+            raise ValidationError('Текст содержит недопустимые символы 123456')
+        return content
