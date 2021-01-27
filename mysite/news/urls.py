1bd5c5069a9527c1b41ee79fbf64836c01cd8a45
AlexanderSeryoginUpwork <alexander.seryogin.upwork@gmail.com>
2021-01-27 16:32:41 +0200

    part1_27__Класс ListView. Часть 1

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'),
+    # path('', index, name='home'),
+    path('', HomeNews.as_view(), name='home'),
     path('category/<int:category_id>/', get_category, name='category'),
     path('news/<int:news_id>', view_item2, name='view_item3'),
     path('news/add', add_news, name='add_news'),
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -3,16 +3,21 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from .models import News, Category
 from .forms import NewsForm
+from django.views.generic import ListView
 
 
-def index(request):
-    news = News.objects.all()
-    categories = Category.objects.all()
-    context = {
-        'news': news,
-        'title': 'Список новостей'
-    }
-    return render(request, template_name='news/index.html', context=context)
+class HomeNews(ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(is_published=True)
 
 
 def get_category(request, category_id):
@@ -33,7 +38,8 @@ def add_news(request):
     if request.method == 'POST':
         form = NewsForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data) # {'title': 'Новость из формы', 'content': 'Новость из формы 123', 'is_published': True, 'category': <Category: Наука>}
+            print(
+                form.cleaned_data)  # {'title': 'Новость из формы', 'content': 'Новость из формы 123', 'is_published': True, 'category': <Category: Наука>}
             # news = News.objects.create(**form.cleaned_data)
             news = form.save()
             return redirect(news)
